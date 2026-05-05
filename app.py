dd9fda1589c6fba86f6e2545084636136cb8ac71
farukomer44-ui <farukomer.44@gmail.com>
2026-05-05 09:56:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ import sqlite3
 from datetime import date, timedelta
 import math  # <--- BU SATIRI EKLEYİN
 
+import os
+
+database_url = os.environ.get("DATABASE_URL")
+
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+
 app = Flask(__name__)
 app.secret_key = "kpss_2026_farukomer_guvenlik"
 
